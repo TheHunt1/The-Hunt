0cfeddbb7c2c19a4f7a9c6f259c2f8c1a42d69b8
draca3565 <troyknife@gmail.com>
2017-06-02 16:54:53 -0600

    start week 7 assignment

---
+++ TheHunt/Hunt/src/byui/cit260/theHunt/view/MainMenuView.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.view;
+
+/**
+ *
+ * @author Dragon
+ */
+public class MainMenuView {
+    private String menu;
+    
+    public MainMenuView (){
+        this.menu= "\n"
+                + "\n-------------------------------"
+                + "\n   Main Menu                   "
+                + "\n-------------------------------"
+                + "\nN - Start new game"
+                + "\nG - Get and start saved game"
+                + "\nH - Get help on how to play"
+                + "\nS - Save game"
+                + "\nQ - Quit"
+                + "\n-------------------------------";
+    }
+    void displayMainMenuView() {
+        boolean done = false; //set flag to not done
+       do {
+           //prompt for and get player's name
+           String menuOption = this.getmenuOption();
+           if(menuOption.toUpperCase().equals("Q"))
+               return;
+           
+           done = this.doAction(menuOption);
+       }
+       while (!done);
+    }
+    
+}
