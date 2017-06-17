5896313fcfba16827af23dc5232394a8d16c6e09
MW <MW@Waiolama-PC>
2017-06-16 19:55:55 -0700

    6/16/17 commit by Mitchell

---
+++ TheHunt/Hunt/src/byui/cit260/theHunt/view/WeaponStoreMenuView.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHunt.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MW
+ */
+public class WeaponStoreMenuView {
+    private String menu;
+    
+    public WeaponStoreMenuView () {
+        this.menu = ("\n"
+                + "\n---------------------------------"
+                + "\n      Weapons Store              "
+                + "\n---------------------------------"
+                + "\nW - Weapons list (including cost)"
+                + "\nP - Purchase weapon"
+                + "\nB - Back");   
+    }
+    
+    public void displayWeaponStoreMenuView() {
+        boolean done = false; //set flag to not done
+       do {
+           //prompt for and get player's name
+           String menuOption = this.getMenuOption();
+           if(menuOption.toUpperCase().equals("Q"))
+               return;
+           
+           done = this.doAction(menuOption);
+       }
+       while (!done);
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\nEnter your choice");
+
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if(value.length() < 1) {
+                System.out.println("\nInvalid value; value can't be blank");
+                continue;
+                
+            }
+            break;
+        }
+        return value;
+    }
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "W":
+                System.out.println("\n*** Display list of available weapons"
+                + "\nincluding the price. ***");
+                break;
+            case "P":
+                System.out.println("\n*** Use the money you earned from your kills "
+                + "\nto purchase a weapons. Here you will select and purchase "
+                + "\nweapons. ***");
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return false;
+    }
+    
+}
